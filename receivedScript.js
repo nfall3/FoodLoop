fab254983f138402a328c4505f819e118c62b2a1
Sabua204 <sabual2@lsu.edu>
2025-11-20 12:09:06 -0600

    Almost complete, commiting so that linking after submitting can be done by those who have access to database.
    Need to update js to react since just importing react cause all functionality to stop working, also working on finding a way to get character count to work inside the text box for coments.
    
    Managed to fix error where date for expiration wouldn't appear when selecting no if expiration date doesn't match what was advertised.
    
    Also updated nav links to match for when we separate accounts between user and organization as well as linking to home. Besides this, I also added a small change to nav to prevent it from being scrollable and always keeping it on the page

--- receivedScript.js
+++ receivedScript.js
@@ -1,3 +1,4 @@
+
 function showSideBar(){
     const sideBar = document.querySelector('.sideBar')
     sideBar.style.display= 'flex'
@@ -10,7 +11,7 @@ function hideSidebar(){
 
 
  //rewriting because cant find issue
- /*
+ 
 
 function updateForm(){
 
@@ -19,22 +20,18 @@ function updateForm(){
          const date = document.getElementById('actualExpirationContainor');
         const expiration = document.getElementById('actualExpiration');
 
-         if(no.checked == true){
+          if(no.checked){
                 date.style.display = 'block';
             }else{
                 date.style.display = 'none';
             }
     }
 
-    document.querySelectorAll('input name="expirationDate').forEach(radio => {
+    document.querySelectorAll('input[name="expirationDate"]').forEach(radio => { //Error was found here, there was a space between 'input' and '['
         radio.addEventListener('change', updateForm);
            
         });
         
-            updateForm(); // called here to set initial state of field
-
-})
-
-*/
+            updateForm(); // called to set initial state of field
 
 
