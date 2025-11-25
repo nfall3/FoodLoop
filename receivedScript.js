26b22faaffbab5b217fb6032acd80b5d6e012ccb
Sabua204 <sabual2@lsu.edu>
2025-11-25 13:10:50 -0600

    updated received form with issue

--- receivedScript.js
+++ receivedScript.js
@@ -1,3 +1,8 @@
+/*
+//react is only used for character count function
+import { useState } from "react";
+import { useEffect } from "react";
+*/
 
 function showSideBar(){
     const sideBar = document.querySelector('.sideBar')
@@ -11,6 +16,7 @@ function hideSidebar(){
 
 
  //rewriting because cant find issue
+ //UPDATE: ISSUE FOUND 
  
 
 function updateForm(){
@@ -35,3 +41,41 @@ function updateForm(){
             updateForm(); // called to set initial state of field
 
 
+
+
+//ISSUE            
+//Likely because i tried to implement with react since the professor called for it suddenly:
+//Funtionality stops working
+//WHAT WAS ATTEMPTED HERE:
+//Adding a character counter for the comment text box
+//Commented out for now
+//Some parts also ommited in receivedMain.html because of this issue.
+/*
+function CharacterCounter(){
+    const [count, setCount] = React.useState(0);
+
+    React.useEffect(() => {
+        const textarea = document.getElementById('free-text');
+
+        if(!textarea){
+            return;
+        }
+
+        const updateCount = () =>{
+            setCount(textarea.value.length);
+        };
+
+        textarea.addEventListener("input", updateCount);
+        count();
+
+        return () => textarea.removeEventListener("input", updateCount);
+    },
+    );
+
+    return(
+        <div>{count}/300</div>
+    );
+}
+
+ReactDom.createRoot(document.getElementById("react-root")).render(<CharacterCounter/>);
+*/
